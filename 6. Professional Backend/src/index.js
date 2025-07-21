b4c3fd28de8d876512611c32c0e2870c844d4d4e
KrishModi0610 <krishmodi0610@gmail.com>
2025-07-21 11:23:48 +0530

    Connect to MongoDB

---
+++ 6. Professional Backend/src/index.js
@@ -0,0 +1,47 @@
+// require('dotenv').config({path: './env'})
+// import mongoose from "mongoose"
+// import { DB_NAME } from "./constants";
+
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+
+dotenv.config({
+   path: './env'
+})
+connectDB(); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import express from "express"
+const app = express();
+
+;(async () => {
+   try {
+      await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+      app.error("error", (error) => {
+         console.error("Error", error)
+         throw error;
+      })
+
+      app.listen(`${process.env.PORT}`, ())
+   } catch (error) {
+      console.error("Error", error);
+      throw error;
+   }
+})()
+
+*/
